f0ffee670b60d08957d7df57ce471e951e054429
voron00 <alexey12424323@gmail.com>
2018-06-10 22:59:16 +0300

    zl1: add zl0 firmwares

--- zl1-vendor.mk
+++ zl1-vendor.mk
@@ -133,6 +133,19 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/zl1/proprietary/etc/firmware/rampatch_tlv_2.1.tlv:system/etc/firmware/rampatch_tlv_2.1.tlv \
     vendor/leeco/zl1/proprietary/etc/firmware/rampatch_tlv_3.0.tlv:system/etc/firmware/rampatch_tlv_3.0.tlv \
     vendor/leeco/zl1/proprietary/etc/firmware/rampatch_tlv_3.2.tlv:system/etc/firmware/rampatch_tlv_3.2.tlv \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530_gpmu.fw2:system/etc/firmware/zl0/a530_gpmu.fw2 \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530_pfp.fw:system/etc/firmware/zl0/a530_pfp.fw \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530_pm4.fw:system/etc/firmware/zl0/a530_pm4.fw \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530_zap.b00:system/etc/firmware/zl0/a530_zap.b00 \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530_zap.b01:system/etc/firmware/zl0/a530_zap.b01 \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530_zap.b02:system/etc/firmware/zl0/a530_zap.b02 \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530_zap.elf:system/etc/firmware/zl0/a530_zap.elf \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530_zap.mdt:system/etc/firmware/zl0/a530_zap.mdt \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530v1_pfp.fw:system/etc/firmware/zl0/a530v1_pfp.fw \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530v1_pm4.fw:system/etc/firmware/zl0/a530v1_pm4.fw \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530v2_seq.fw2:system/etc/firmware/zl0/a530v2_seq.fw2 \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530v3_gpmu.fw2:system/etc/firmware/zl0/a530v3_gpmu.fw2 \
+    vendor/leeco/zl1/proprietary/etc/firmware/zl0/a530v3_seq.fw2:system/etc/firmware/zl0/a530v3_seq.fw2 \
     vendor/leeco/zl1/proprietary/etc/permissions/ConnectivityExt.xml:system/etc/permissions/ConnectivityExt.xml \
     vendor/leeco/zl1/proprietary/etc/permissions/cneapiclient.xml:system/etc/permissions/cneapiclient.xml \
     vendor/leeco/zl1/proprietary/etc/permissions/com.qti.dpmframework.xml:system/etc/permissions/com.qti.dpmframework.xml \
